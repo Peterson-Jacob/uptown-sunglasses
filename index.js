ab282b601eb6d3f9898717ca147a295ac3e81779
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2022-08-26 16:21:33 -0500

    Update index.js
    playing around with functionality. not committed with animation at moment.

--- index.js
+++ index.js
@@ -3,5 +3,9 @@ const tl = gsap.timeline({defaults:{duration: 1}});
 
 
 if(document.body.offsetWidth > 576){
-    tl.from('.action', {y: "random(-250, 250)", opacity: 0}, "+=.25")
+    //tl.to('.move', 1, {width: '60%', ease: Power2.easeOut})
+   // tl.to('.action', {display: 'none'})
+   //tl.to('.nav-about-open', {display: 'block'})
+   tl.from('.about', 1, {x: '-500', opacity: 0})
+    
 }
