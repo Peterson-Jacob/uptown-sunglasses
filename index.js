a67f77b0d6351e3cab38a4cfc8d310ce5aecfe0a
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2022-08-31 16:15:27 -0500

    Update index.js
    Working on GSAP animation.

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
+window.onload = () =>{
+
 const abtBtn = document.querySelector('.aboutBtn');
 const abtOpen = document.querySelector('.nav-about-open');
 const dlBtn = document.querySelector('.dealsBtn');
-const dlOoen = document.querySelector('.nav-deals-open');  
+const dlOpen = document.querySelector('.nav-deals-open');
+const front = document.querySelector('.move');  
 
 
 
 const tl = gsap.timeline({defaults:{duration: 1}, paused: true, reversed: true});
+const tl2 = gsap.timeline({defaults:{duration: 1}, paused: true, reversed: true});
 
+const x = "deals";
+const y = "about";
 
-
-function Start(){
-
-  tl.add('start')
-  tl.to('.move', 1, {width: '60%', ease: Power2.easeOut},'start')
-  tl.to('.action', {width: '80%'}, 'start')
-  tl.to('.list-item', { margin: 'auto'}, 'start')
-  tl.to('.nav-deals-open', {display: 'block'})
-  tl.from( '.deals', 1, {x: '-500', opacity: 0})
+function Start(t){
+    t.add('start')
+    t.to(front, 1, {width: '60%', ease: Power2.easeOut},'start')
+    t.to('.action', {width: '80%'}, 'start')
+    t.to('.list-item', { margin: 'auto'}, 'start')
+   // tl.to('.nav-' + y + '-open', {display: 'block'})
 }
+
 
 if(document.body.offsetWidth > 576 && document.body.offsetWidth < 991){
 
@@ -29,27 +33,31 @@ if(document.body.offsetWidth > 576 && document.body.offsetWidth < 991){
 }
 
   if(document.body.offsetWidth >= 992){
-      Start();
- }
- 
- abtBtn.addEventListener('click', () =>{
-  active()
-  toggleTween(tl)
 
-  let x = "about";
-  
+    Start(tl);
+    tl.to('.nav-' + y + '-open', {display: 'block'})
+    tl.from( '.' + y , 1, {x: '-500', opacity: 0}) 
+
+    Start(tl2);
+    tl2.to('.nav-' + x + '-open', {display: 'block'})
+    tl2.from( '.' + x , 1, {x: '-250', opacity: 0}) 
+}
+
+ abtBtn.addEventListener('click', () =>{
+  active(tl)
+  toggleTween(tl)
 
 })
 
 dlBtn.addEventListener('click', () =>{
-  active()
-  toggleTween(tl)
   
-  
+  active(tl2)
+  toggleTween(tl2)
+   
 })
 
-function active(){
-  if(tl.isActive() == true){
+function active(z){
+  if(z.isActive() == true){
     e.preventDefault();
     e.stopImmediatePropagation();
     return false;
@@ -58,5 +66,23 @@ function active(){
 }
 
 function toggleTween(tween){
-  tween.reversed() ? tween.play() : tween.reverse();
+  
+  if(tween == tl){
+    tl2.reverse({delay: 1})
+    tl.reversed() ? tween.play() : tween.reverse();
+   
+  }
+
+  if(tween == tl2){
+    
+    tl.reverse({delay: 1})
+    tl2.reversed() ? tween.play() : tween.reverse();
+  }
+
+  
+  
+  //tween.reversed() ? tween.play() : tween.reverse();
+  
+}
+
 }
