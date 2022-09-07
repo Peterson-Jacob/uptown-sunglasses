f47bb705a6ea9e127c97f7d8edd29f092eba6757
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2022-09-07 16:09:10 -0500

    Update index.js
    Got the section animations working allowing for smooth animations between sections.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const ckOpen = document.querySelector('.nav-checkout-open');
 const front = document.querySelector('.move');  
 
 
-
+const t = gsap.timeline({defaults:{duration: 1}, paused: true, reversed: true});
 const tl = gsap.timeline({defaults:{duration: 1}, paused: true, reversed: true});
 const tl2 = gsap.timeline({defaults:{duration: 1}, paused: true, reversed: true});
 const tl3 = gsap.timeline({defaults:{duration: 1}, paused: true, reversed: true});
@@ -21,63 +21,35 @@ const y = "about";
 const q = "shop";
 const c = "checkout";
 
-function Start(t){
+if(document.body.offsetWidth > 576 && document.body.offsetWidth < 991){
 
-     t.add('start')
-     t.to(front, 1, {width: '60%', ease: Power2.easeOut},'start')
-     t.to('.action', {width: '80%'}, 'start')
-     t.to('.list-item', { margin: 'auto'}, 'start')
-   //tl.to('.nav-' + y + '-open', {display: 'block'})
-}
-
-function Change(t){
   t.add('change')
   t.to('.jumbotron', 1, {opacity: 0} )
   t.to('.jumbotron', {display: 'none'}, 'change')
+  tl.to('.nav-about-open', {display: 'block'})
+  tl.from('.about', {opacity: 0})
+  tl2.to('.nav-deals-open', {display: 'block'})
+  tl2.from('.deals', {opacity: 0})
+  tl3.to('.nav-shop-open', {display: 'block'})
+  tl3.from('.shop', {opacity: 0})
+  tl4.to('.nav-checkout-open', {display: 'block'})
+  tl4.from('.checkout', {opacity: 0})
 }
-
-
-if(document.body.offsetWidth > 576 && document.body.offsetWidth < 991){
-
-  Change(tl);
-  tl.to('.nav-' + y + '-open', {display: 'block'})
-  tl.from('.' + y, {opacity: 0})
-  Change(tl2);
-  tl2.to('.nav-' + x + '-open', {display: 'block'})
-  tl2.from('.' + x, {opacity: 0})
-  Change(tl3);
-  tl3.to('.nav-' + q + '-open', {display: 'block'})
-  tl3.from('.' + q, {opacity: 0})
-  Change(tl4);
-  tl4.to('.nav-' + c + '-open', {display: 'block'})
-  tl4.from('.' + c, {opacity: 0})
-
-
-}
-
 
   if(document.body.offsetWidth >= 992){
 
-    // t.add('start')
-    // t.to(front, 1, {width: '60%', ease: Power2.easeOut},'start')
-    // t.to('.action', {width: '80%'}, 'start')
-    // t.to('.list-item', { margin: 'auto'}, 'start')
-    
-    Start(tl);
-    tl.to('.nav-' + y + '-open', {display: 'block'})
-    tl.from( '.' + y , 1, {x: '-500', opacity: 0}) 
-
-    Start(tl2);
-    tl2.to('.nav-' + x + '-open', {display: 'block'})
-    tl2.from( '.' + x , 1, {x: '-250', opacity: 0}) 
-
-    Start(tl3);
-    tl3.to('.nav-' + q + '-open', {display: 'block'})
-    tl3.from( '.' + q , 1, {x: '-250', opacity: 0}) 
-
-    Start(tl4);
-    tl4.to('.nav-' + c + '-open', {display: 'block'})
-    tl4.from( '.' + c , 1, {x: '-250', opacity: 0}) 
+    t.add('start')
+    t.to(front, 1, {width: '60%', ease: Power2.easeOut},'start')
+    t.to('.action', {width: '80%'}, 'start')
+    t.to('.list-item', { margin: 'auto'}, 'start')
+    tl.to('.nav-about-open', {display: 'block'})
+    tl.from( '.about' , 1, {x: '-500', opacity: 0}) 
+    tl2.to('.nav-deals-open', {display: 'block'})
+    tl2.from( '.deals'  , 1, {x: '-250', opacity: 0}) 
+    tl3.to('.nav-shop-open', {display: 'block'})
+    tl3.from( '.shop' , 1, {x: '-250', opacity: 0}) 
+    tl4.to('.nav-checkout-open', {display: 'block'})
+    tl4.from( '.checkout' , 1, {x: '-250', opacity: 0}) 
 }
 
  abtBtn.addEventListener('click', () =>{
@@ -121,54 +93,65 @@ function toggleTween(tween){
   
   if(tween == tl){
     
-  
+     if(tl.reversed() == false){
+       t.reverse()
+       
+     }
+     if(tl.reversed() == true){
+      t.play()
+    
+    }
 
-    // if(t.reversed(false)){
-    //   t.play()
-    //   tl2.reverse()
-    // }
-
-    // if(t.reversed(true)){
-    //   t.reverse()
-    // }
- 
-   // t.play()
-    tl2.reverse()
-    tl3.reverse()
-    tl4.reverse()
-    //t.reversed() ? t.play() : t.reverse();
-   // t.reversed() ? t.play() : t.reverse();
+    tl2.reverse(true)
+    tl3.reverse(true)
+    tl4.reverse(true)
     tl.reversed() ? tween.play() : tween.reverse();
    
   }
 
   if(tween == tl2){
 
+    if(tl2.reversed() == false){
+      t.reverse()
+    }
+    if(tl2.reversed() == true){
+     t.play()
+   }
    
-    tl.reverse()
-    tl3.reverse()
-    tl4.reverse()
-    //t.reversed() ? t.play() : t.reverse()
-     //t.play()//
+    tl.reverse(true)
+    tl3.reverse(true)
+    tl4.reverse(true)
     tl2.reversed() ? tween.play() : tween.reverse();  
   }
 
   if(tween == tl3){
-    tl.reverse()
-    tl2.reverse()
-    tl4.reverse()
+
+    if(tl3.reversed() == false){
+      t.reverse()
+    }
+    if(tl3.reversed() == true){
+     t.play()
+   }
+
+    tl.reverse(true)
+    tl2.reverse(true)
+    tl4.reverse(true)
     tl3.reversed() ? tween.play() : tween.reverse(); 
   }
 
   if(tween == tl4){
-    tl.reverse()
-    tl2.reverse()
-    tl3.reverse()
-    tl4.reversed() ? tween.play() : tween.reverse(); 
-  }
+    
+    if(tl4.reversed() == false){
+      t.reverse()
+  
+    }
+    if(tl4.reversed() == true){
+     t.play()
+   }
 
-  
-  
-  //tween.reversed() ? tween.play() : tween.reverse();
-  
+    tl.reverse(true)
+    tl2.reverse(true)
+    tl3.reverse(true)
+    tl4.reversed() ? tween.play() : tween.reverse(); 
+  }  
 }
