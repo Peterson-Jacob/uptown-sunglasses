e4a4bf2ba14fce7d92673dfda9a5ad6ac31a5598
Jacob Peterson <67656804+Peterson-Jacob@users.noreply.github.com>
2022-09-12 16:35:35 -0500

    Update purchase.js

--- purchase.js
+++ purchase.js
@@ -6,6 +6,16 @@ window.onload = () =>{
     const yPurchase = document.getElementsByClassName('yBtn');
     const aPurchase = document.getElementsByClassName('aBtn');
     const cart = document.getElementById("cart-items");
+    const check = document.getElementById("checkBtn");
+    const emptyMessage = document.getElementById('cartEmpty');
+
+    check.onclick = () =>{
+        if(amt.value == "0"){
+            emptyMessage.textContent = "Cart is empty";
+           }else{
+            tl5.play(true);
+        }
+     }
 
 
     let zCount = 0;
@@ -35,7 +45,7 @@ window.onload = () =>{
         zDiv.appendChild(zMod);
         zMod.textContent = "Z-Model"
         zDiv.appendChild(zQty);
-        zQty.textContent = "quanity " + quanity;
+        zQty.textContent = "Quantity " + quanity;
         zQty.id = "zCountid";
         zDiv.appendChild(zPrice);
         zPrice.textContent = " Price $" + itemPrice;
@@ -43,19 +53,24 @@ window.onload = () =>{
         zDiv.appendChild(zBut);
 		zBut.textContent = 'Remove';
         cart.appendChild(zDiv);
+        emptyMessage.textContent = "";
         }
         if(zCount > 1){
-           document.getElementById('zCountid').textContent = "Amount " + quanity; 
+           document.getElementById('zCountid').textContent = "Quantity " + quanity; 
            document.getElementById('zPriceid').textContent = " Price $" + itemPrice;
         }
 
         zBut.onclick = function( ){
             amt.value = amt.value - (zCount * price);
             zCount = 0;
-          
             zDiv.remove();
-
         }
+
+        check.addEventListener('click', () =>{
+            amt.value = amt.value - (zCount * price);
+            zCount = 0;
+            zDiv.remove();
+        })
     }
 
     
@@ -78,9 +93,9 @@ window.onload = () =>{
         xDiv.appendChild(xImg);
         xImg.src = 'images/glasses4.jpg';
         xDiv.appendChild(xMod);
-        xMod.textContent = "x-Model"
+        xMod.textContent = "X-Model"
         xDiv.appendChild(xQty);
-        xQty.textContent = "quanity " + quanity;
+        xQty.textContent = "Quantity " + quanity;
         xQty.id = "xCountid";
         xDiv.appendChild(xPrice);
         xPrice.textContent = " Price $" + itemPrice;
@@ -88,20 +103,25 @@ window.onload = () =>{
         xDiv.appendChild(xBut);
 		xBut.textContent = 'Remove';
         cart.appendChild(xDiv);
+        emptyMessage.textContent = "";
         }
         if(xCount > 1){
-           document.getElementById('xCountid').textContent = "Amount " + quanity; 
+           document.getElementById('xCountid').textContent = "Quantity " + quanity; 
            document.getElementById('xPriceid').textContent = " Price $" + itemPrice;
         }
 
         
         xBut.onclick = function( ){
             amt.value = amt.value - (xCount * price);
-            xCount = 0;
-          
+            xCount = 0; 
             xDiv.remove();
-
         }
+
+        check.addEventListener('click', () =>{
+            amt.value = amt.value - (xCount * price);
+            xCount = 0;
+            xDiv.remove();
+        })
     }
 
     zPurchase[0].onclick = () =>{
@@ -145,7 +165,7 @@ window.onload = () =>{
         yDiv.appendChild(yMod);
         yMod.textContent = "Y-Model"
         yDiv.appendChild(yQty);
-        yQty.textContent = "quanity " + quanity;
+        yQty.textContent = "Quantity " + quanity;
         yQty.id = "yCountid";
         yDiv.appendChild(yPrice);
         yPrice.textContent = " Price $" + itemPrice;
@@ -153,21 +173,28 @@ window.onload = () =>{
         yDiv.appendChild(yBut);
 		yBut.textContent = 'Remove';
         cart.appendChild(yDiv);
+        emptyMessage.textContent = "";
+    
         }
         if(yCount > 1){
-           document.getElementById('yCountid').textContent = "Amount " + quanity; 
+           document.getElementById('yCountid').textContent = "Quantity " + quanity; 
            document.getElementById('yPriceid').textContent = " Price $" + itemPrice;
         }
 
         
         yBut.onclick = function( ){
             amt.value = amt.value - (yCount * price);
-            yCount = 0;
-          
+            yCount = 0; 
             yDiv.remove();
-
         }
+        check.addEventListener('click', () =>{
+            amt.value = amt.value - (yCount * price);
+            yCount = 0;
+            yDiv.remove();
+        })
+
     }
+
     aPurchase[0].onclick = () =>{
         aCount += 1;
         const quanity = (0 + aCount);
@@ -189,7 +216,7 @@ window.onload = () =>{
         aDiv.appendChild(aMod);
         aMod.textContent = "A-Model"
         aDiv.appendChild(aQty);
-        aQty.textContent = "quanity " + quanity;
+        aQty.textContent = "Quantity " + quanity;
         aQty.id = "aCountid";
         aDiv.appendChild(aPrice);
         aPrice.textContent = " Price $" + itemPrice;
@@ -197,9 +224,10 @@ window.onload = () =>{
         aDiv.appendChild(aBut);
 		aBut.textContent = 'Remove';
         cart.appendChild(aDiv);
+        emptyMessage.textContent = "";
         }
         if(aCount > 1){
-           document.getElementById('aCountid').textContent = "Amount " + quanity; 
+           document.getElementById('aCountid').textContent = "Quantity " + quanity; 
            document.getElementById('aPriceid').textContent = " Price $" + itemPrice;
         }
 
@@ -207,10 +235,14 @@ window.onload = () =>{
         aBut.onclick = function( ){
             amt.value = amt.value - (aCount * price);
             aCount = 0;
-          
             aDiv.remove();
-
         }
+
+        check.addEventListener('click', () =>{
+            amt.value = amt.value - (aCount * price);
+            aCount = 0;
+            aDiv.remove();
+        })
 
         }
     }
